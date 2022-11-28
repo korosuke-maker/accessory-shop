7d2a346e83f9d80eb136cb617c4f804a9ec102e9
korosuke <99318290+korosuke-maker@users.noreply.github.com>
2022-11-28 18:11:52 +0900

    最新のデータを更新

---
+++ js/script.js
@@ -0,0 +1,75 @@
+$(function(){
+    //drawer
+    $('.drawer').drawer();
+
+
+    //smoothscroll
+    // #から始まるURLがクリックされた時
+jQuery('a[href^="#"]').click(function() {
+    // 移動速度を指定（ミリ秒）
+    let speed = 300;
+    // hrefで指定されたidを取得
+    let id = jQuery(this).attr("href");
+    // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
+    let target = jQuery("#" == id ? "html" : id);
+    // ページのトップを基準にターゲットの位置を取得
+    let position = jQuery(target).offset().top;
+    // ターゲットの位置までspeedの速度で移動
+    jQuery("html, body").animate(
+      {
+        scrollTop: position - $('#js-header').outerHeight()
+     },
+      speed
+    );
+    return false;
+  });
+
+  //wow
+  new WOW().init();
+
+  //googleform
+  let $form = $('#js-form')
+  $form.submit((e) => {
+          $.ajax({
+              url: $form.attr('action'),
+              data: $form.serialize(),
+              type: "POST",
+              dataType: "xml",
+              statusCode: {
+                  0: function () {
+                      //送信に成功したときの処理
+                      $form.slideUp();
+                      $('#js-success').slideDown();
+                  },
+                  200: function () {
+                      //送信に失敗したときの処理
+                      $form.slideUp();
+                      $('#js-error').slideDown();
+                  }
+              }
+          });
+          return false;
+      });
+
+  //contact-formの入力確認
+  let $submit = $('#js-submit')
+  $('#js-form input, #js-form textarea').on('change',function(){
+      if(
+          $('#js-form input[type="text"]').val() !=="" &&
+          $('#js-form input[type="email"]').val() !=="" &&
+          $('#js-form input[name="entry.1718789866"]').prop('checked') ===true
+      ) {
+          //すべてフォームが入力された時
+        $submit.prop('disabled',false)
+        $submit.addClass('-active')
+      }else{
+        //フォームに入力されてないものがある時
+        $submit.prop('disabled',true)
+        $submit.removeClass('-active')
+      }
+  })
+
+
+
+  
+})
